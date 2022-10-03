9c353e0a39ba4768915b81f31f478c2196dfcf7f
Brendan Rogan <brendan.rogan@kcl.ac.uk>
2022-10-03 16:23:27 +0100

    Use simple primality algorithm instead

--- primes.py
+++ primes.py
@@ -1,11 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-from sympy import prime
+
+def is_prime(n):
+    if n <= 3:
+        return n > 1
+    if not n%2 or not n%3:
+        return False
+    i = 5
+    stop = int(n**0.5)
+    while i <= stop:
+        if not n%i or not n%(i + 2):
+            return False
+        i += 6
+    return True
 
 def primes(number_of_primes):
     list = []
 
-    for i in range(1, number_of_primes+1):
-        list.append(prime(i))
+    i = 0
+    while len(list) < number_of_primes:
+        if is_prime(i):
+            list.append(i)
+        i += 1
 
     return list
+
