b6c38c618d6bb1b722db68cefcaf792865eb5d6c
Brendan Rogan <brendan.rogan@kcl.ac.uk>
2022-10-03 15:45:14 +0100

    Use sympy library to print prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,11 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+from sympy import prime
 
 def primes(number_of_primes):
     list = []
+
+    for i in range(1, number_of_primes+1):
+        list.append(prime(i))
+
     return list
